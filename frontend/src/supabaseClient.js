e455cf3c6e31061c4a2b425df77f38df4b7273bd
Brian Leong <66709977+BrianStormDev@users.noreply.github.com>
2025-06-21 19:30:07 -0700

    Restart

--- frontend/src/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://rgsntbrcaiuvxznjukyi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnc250YnJjYWl1dnh6bmp1a3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MzA2MjMsImV4cCI6MjA2NjEwNjYyM30.2_qDAl4Sh3n8ujjpYWM52g-HgZyfcuLpW8jsnD6mxcE';
-export const supabase = createClient(supabaseUrl, supabaseKey);
